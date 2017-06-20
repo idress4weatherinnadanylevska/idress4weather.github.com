ec6b30dca1beaaa29e8b4205b769b03a6b2d70e8
idress4weather <danylevskainnassik@gmail.com>
2017-06-20 12:05:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(function () {
+
+  var temperature_c = '';
   
 // https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/49.5638034,34.4923746.json
 
   var coordinates = '';
   var url;
-  var temperature_c[];
+  
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       coordinates+=position.coords.latitude + ',' + position.coords.longitude;
@@ -14,7 +16,7 @@ $(function () {
       // send request to the weather server
       $.ajax( {
         url: url,
-        async: false,
+        //async: false,
         success: function(data) {
 
           temperature_c = data.current_observation.temp_c + " <a id='temp_unit_c'>C</a>"
@@ -40,8 +42,50 @@ $(function () {
 
           $("#weather").html(data.current_observation.weather); 
 
+//temperature_c = parseString(temperature_c);//+++++++++
+// запускаем функцию в будущее на 10 секунд
+//window.setTimeout(function() {
+var userFeed = new Instafeed({
+get: 'user',
+userId: '2321088784',
+accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
+    template: '<a href="{{link}}"><img src="{{image}}" /></a>',
+     limit: 60,
+//async: false,
+tags: temperature_c,
+
+    success: function() {
+        foundImages = 0;
+        maxImages = 5;
+    },
+    // запускаем функцию в будущее на 10 секунд
+    filter: window.setTimeout(function(image) {
+     //return image.tags.indexOf('25') >= 0;    
+     if (image.tags.indexOf(temperature_c) >= 0 && foundImages < maxImages) {
+            foundImages = foundImages + 1;
+            return true;
+           
+        }
+        
 
 
+    //else if(image.tags.indexOf('23') >= 0 && foundImages < maxImages) {
+      //      foundImages = foundImages + 1;
+        //    return true;
+        //}
+    //else if(image.tags.indexOf('25') >= 0 && foundImages < maxImages) {
+      //      foundImages = foundImages + 1;
+        //    return true;
+        //} 
+
+        return false;
+         //alert(temperature_c);
+  } 
+
+  10000);
+  //alert(my_superglobal);    
+});
+userFeed.run();
 
 
         },
@@ -49,39 +93,9 @@ $(function () {
       });       
       
     });
-  }    
+  } 
 
 
 
-var userFeed = new Instafeed({
-get: 'user',
-userId: '2321088784',
-accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
-    template: '<a href="{{link}}"><img src="{{image}}" /></a>',
-     limit: 60,
-async: false,
-tags: temperature_c,
-    success: function() {
-        foundImages = 0;
-        maxImages = 5;
-    },
-    filter: function(image) {
-     //return image.tags.indexOf('25') >= 0;    
-     if (image.tags.indexOf(temperature_c) >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }
-    else if(image.tags.indexOf('23') >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }
-    else if(image.tags.indexOf('25') >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }    
-        return false;
-  }    
-});
-userFeed.run();
 
 });
