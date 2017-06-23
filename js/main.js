ff49e80e9d098a8602b4231e8c3592d79475bfbf
idress4weather <danylevskainnassik@gmail.com>
2017-06-23 09:29:28 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(function() {
 function ajaxRequest() {
   var temperature_c = false;
@@ -28,55 +27,36 @@ function ajaxRequest() {
          },
         cache: false
       });       
-      return temperature_c;
+      //
     });
   }    
+
+
+return temperature_c;
+}
 var something = ajaxRequest();
 
- 
-
-
-
-
-window.setTimeout(function() {      
-
-      var userFeed = new Instafeed({
+var userFeed = new Instafeed({
 get: 'user',
 userId: '2321088784',
 accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
     template: '<a href="{{link}}"><img src="{{image}}" /></a>',
      limit: 60,
-async: false,
-tags: something,
+     tags: something,
+    //data: {access_token: tok, count: kolichestvo},//+++++++++++
     success: function() {
         foundImages = 0;
         maxImages = 5;
     },
     filter: function(image) {
-      //var c = temperature_c = data.current_observation.image.tags.indexOf;
-      //if(c == '25'){ 
-     //return image.tags.indexOf('25') >= 0;} 
-     //else if(c == '24'){ 
-     //return image.tags.indexOf('24') >= 0;}
-     //else if(c == '23'){ 
-     //return image.tags.indexOf('23') >= 0;}   
-     if (image.tags.indexOf(something) >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }
-    
-               return false;
-             
-});//user
-//alert(temperature_c);
-  } 
+        if(something == '25')
+           return image.tags.indexOf('25') >= 0;
+        
+      
+  }    
 
-  10000);
-  //alert(my_superglobal);
-userFeed.run();
-//bo object
-//$("#instafeed").html(userFeed);
+});
+userFeed.run(); 
+}); 
 
 
-  
-});//$func  
