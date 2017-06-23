10a7ece3de0cb99b207e43b04c79fd9e075e6b17
idress4weather <danylevskainnassik@gmail.com>
2017-06-23 10:23:09 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,24 @@
-function ajaxRequest() {
-  var temperature_c = false;
-$(function() {
-
-  
+$(function () {
   
 // https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/49.5638034,34.4923746.json
 
   var coordinates = '';
   var url;
- 
+  
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       coordinates+=position.coords.latitude + ',' + position.coords.longitude;
       
       url = 'https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/' + coordinates + '.json'     
       
-   $.ajax({
       // send request to the weather server
-      //$.ajax( {
-     
+      $.ajax( {
         url: url,
-      async: false,
         success: function(data) {
-         temperature_c = data.current_observation.temp_c; 
+
+          var temperature_c = data.current_observation.temp_c + " <a id='temp_unit_c'>C</a>"
           $("#temp_c").html(temperature_c);
-          
+
           $("#temp_unit_c").on('click', function(){
             $("#temp_c").toggle();
             $("#temp_f").toggle();
@@ -45,8 +39,15 @@ $(function() {
 
           $("#weather").html(data.current_observation.weather); 
 
+        },
+        cache: false
+      });       
+      
+    });
+  }    
 
-         },
+}); 
+    
         cache: false
       });       
       //
@@ -54,36 +55,8 @@ $(function() {
   }    
 
 }); 
-return temperature_c;
-}
-var something = ajaxRequest();
 
-var userFeed = new Instafeed({
-get: 'user',
-userId: '2321088784',
-accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
-    template: '<a href="{{link}}"><img src="{{image}}" /></a>',
-     limit: 60,
-     tags: something,
-    //data: {access_token: tok, count: kolichestvo},//+++++++++++
-    success: function() {
-        foundImages = 0;
-        maxImages = 5;
-    },
-    filter: function(image) {
-        //if(something == '25')
-          // return image.tags.indexOf('25') >= 0;
-        if (image.tags.indexOf(something) >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }
-    
-               return false;
-      
-  }    
 
-});
-userFeed.run(); 
 
 
 
