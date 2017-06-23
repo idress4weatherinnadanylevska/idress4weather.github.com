8c7bd78119c3d7adfb3f8618154eb930000ca0f8
idress4weather <danylevskainnassik@gmail.com>
2017-06-23 17:53:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
         //var image.tags = ['25'];
         //var temperature_c1 = data.current_observation.temp_c1;+++++++++
         //==data.current_observation.temp_c1+++++++++++++++++++++
-      if(c == '25'){
+      if(c == '2'){
      if (image.tags.indexOf('25') >= 0 && foundImages < maxImages) {//++++
             foundImages = foundImages + 1;
             return true;
