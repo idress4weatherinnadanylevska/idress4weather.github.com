e0b9d4ff787c42330b8bbcd7310be5d627c5ebbd
idress4weather <danylevskainnassik@gmail.com>
2017-06-23 17:43:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,14 +41,14 @@ $(function () {
           $("#weather").html(data.current_observation.weather); 
           
                  
-            
+         var c = data.current_observation.temp_c;   
         var userFeed = new Instafeed({
 get: 'user',
 userId: '2321088784',
 accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
     template: '<a href="{{link}}"><img src="{{image}}" /></a>',
      limit: 60,
-//tags: temp_c,            
+tags: c,            
             
     //data: {access_token: tok, count: kolichestvo},//+++++++++++
     success: function() {
@@ -64,19 +64,20 @@ accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
         //var image.tags = ['25'];
         //var temperature_c1 = data.current_observation.temp_c1;+++++++++
         //==data.current_observation.temp_c1+++++++++++++++++++++
-     if (image.tags.indexOf('25') >= 0 && foundImages < maxImages) {//++++
+      if(c == '25'){
+     if (image.tags.indexOf(tags) >= 0 && foundImages < maxImages) {//++++
             foundImages = foundImages + 1;
             return true;
-        }
-    else if(image.tags.indexOf('23') >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }
-    else if(image.tags.indexOf('27') >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }    
-        return false;
+         }
+//     else if(image.tags.indexOf(tags) >= 0 && foundImages < maxImages) {
+//             foundImages = foundImages + 1;
+//             return true;
+//         }
+//     else if(image.tags.indexOf('25') >= 0 && foundImages < maxImages) {
+//             foundImages = foundImages + 1;
+//             return true;
+//         }    
+        return false;}
   }    
 
 }); 
