6f0fa2857d6cb9802afad9744f3bb9b8e151873e
idress4weather <danylevskainnassik@gmail.com>
2017-06-26 13:40:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(function () {
   
 // https://api.wunderground.com/api/7f0451b8da14a202/conditions/forecast/q/49.5638034,34.4923746.json
@@ -38,54 +39,6 @@ $(function () {
           $( "#icon" ).attr( "src", iconSrc );
 
           $("#weather").html(data.current_observation.weather); 
-          
-                 
-         var c = data.current_observation.temp_c;   
-        var userFeed = new Instafeed({
-get: 'user',
-userId: '2321088784',
-accessToken: '2321088784.1677ed0.bde13057fffd4b30aad200715ee85b61',
-    template: '<a href="{{link}}"><img src="{{image}}" /></a>',
-     limit: 60,
-tags: c, //  userFeed.tags  this.tags     
-            
-    //data: {access_token: tok, count: kolichestvo},//+++++++++++
-    success: function() {
-        foundImages = 0;
-        maxImages = 5;
-    },
-          window.setTimeout(function() {
-    filter: function(image) {
-        
-         //return image.tags.indexOf('TAG1') >= 0 && image.tags.indexOf('TAG2') >= 0;
-     //return image.tags.indexOf('25') >= 0;
-        
-       // str.indexOf(searchValue[, fromIndex])
-        //var image.tags = ['25'];
-        //var temperature_c1 = data.current_observation.temp_c1;+++++++++
-        //==data.current_observation.temp_c1+++++++++++++++++++++
-          
-          
-//       if(c == '25'){
-//      if (image.tags.indexOf('25') >= 0 && foundImages < maxImages) {//++++
-//             foundImages = foundImages + 1;
-//             return true;
-//          }
-//     else 
-                                     if(image.tags.indexOf(this.tags) >= 0 && foundImages < maxImages) {
-            foundImages = foundImages + 1;
-            return true;
-        }
-//     else if(image.tags.indexOf('25') >= 0 && foundImages < maxImages) {
-//             foundImages = foundImages + 1;
-//             return true;
-//         }    
-        return false;//}
-  }  
-                                     }, 10000);
-
-}); 
-userFeed.run(); 
 
         },
         cache: false
@@ -94,5 +47,5 @@ userFeed.run();
     });
   }    
 
-}); 
+});
 
