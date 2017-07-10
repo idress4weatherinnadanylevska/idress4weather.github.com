0927c8604aff812406f2ad29a1482dd72533088d
idress4weather <danylevskainnassik@gmail.com>
2017-07-10 12:03:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,13 +64,14 @@ accessToken: '5679701317.8f4c5bf.69b3f2c784fe48df9aa9912635f1ffe0',
             // disable button if no more results to load
             if (!this.hasNext()) {
                 nextButton.setAttribute('disabled', 'disabled');
-            }               
-        },
-         after: function () {
-    $(".instagram-image").each(function (i) {
+                  $(".instagram-image").each(function (i) {
         if(i >= 60) {
             $(this).remove();
-        }, 
+            }               
+        },
+        
+
+      
           
           
           
